be0b71f44e3c89f251b514f8d4bd2030606fea2d
Hyagoloyola <hyagoloyolaleite@gmail.com>
2026-02-02 14:26:06 -0300

    Resoluções de desafios

---
+++ Desafios/d010/calcularBhaskar.js
@@ -0,0 +1,5 @@
+function iniciarCalculo(){
+    let a = Number(prompt('Qual é o valor de a?'))
+    let b = Number(prompt('Qual é o valor de b?'))
+    let c = Number(prompt('Qual é o valor de c?'))
+}
